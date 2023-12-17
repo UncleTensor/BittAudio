603bf0616950bcbde0394684b57db8c12f608229
shaukathussain <shaukathussain1@hotmail.com>
2023-12-18 00:49:44 +0100

    Revert "first commit"
    This reverts commit 73e2850e8c8ea7c1e7d481d9552baba45c2ad6f7.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,7 +1,7 @@
 # The MIT License (MIT)
 # Copyright © 2023 Yuma Rao
-# TODO(developer): Set your name
-# Copyright © 2023 <your name>
+# (developer): ETG team
+# Copyright © 2023 ETG
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the “Software”), to deal in the Software without restriction, including without limitation
@@ -19,7 +19,7 @@
 
 # TODO(developer): Change this value when updating your code base.
 # Define the version of the lib module.
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 version_split = __version__.split(".")
 __spec_version__ = (
     (1000 * int(version_split[0]))
@@ -30,4 +30,4 @@ __spec_version__ = (
 # Import all submodules.
 from . import protocol
 from . import reward
-from . import clone_score
+from . import utils
--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,1 @@
 from . import text_to_speech_models
-from . import voice_clone
