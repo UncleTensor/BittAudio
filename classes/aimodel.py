1742c4143b1b1dc8bc9e33ed5782e417cc9d0d0f
shaukathussain <33412791+IamHussain503@users.noreply.github.com>
2024-03-15 10:07:46 +0100

    Reduced the number of request to15 and increased alpha to 0.75
    Reduced the number of request to15 and increased alpha to 0.75

--- classes/aimodel.py
+++ classes/aimodel.py
@@ -46,10 +46,10 @@ class AIModelService:
         parser = argparse.ArgumentParser()
 
         # Add arguments as per your original script
-        parser.add_argument("--alpha", default=0.5, type=float, help="The weight moving average scoring.")
+        parser.add_argument("--alpha", default=0.75, type=float, help="The weight moving average scoring.")
         parser.add_argument("--custom", default="my_custom_value", help="Adds a custom value to the parser.")
         parser.add_argument("--netuid", type=int, default=16, help="The chain subnet uid.")
-        parser.add_argument("--vcdnp", type=int, default=20, help="Number of miners to query for each forward call.")
+        parser.add_argument("--vcdnp", type=int, default=15, help="Number of miners to query for each forward call.")
         parser.add_argument("--max_mse", type=float, default=1000.0, help="Maximum Mean Squared Error for Voice cloning.")
         parser.add_argument("--auto_update", type=str, default='yes', help="Auto update option for github repository updates.")
 
